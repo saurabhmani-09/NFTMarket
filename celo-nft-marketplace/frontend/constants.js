7868953d9bfc78a1fc2d8b5c8ae96250a4e9b5d5
Saurabh Mani <45979194+saurabhmani09@users.noreply.github.com>
2022-08-22 15:56:25 +0530

    uploading NFTMarket

---
+++ celo-nft-marketplace/frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/saurabhmani-09/celo-nftmarketplace";
+
+  export const MARKETPLACE_ADDRESS = "0x3173b8Ea2645a534463c0dcB2B191a523ad0adE7";
